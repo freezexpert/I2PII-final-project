04dafcaa6998d22b1934f590d348506e7f12bb88
freezexpert <55404744+freezexpert@users.noreply.github.com>
2022-01-16 13:25:26 +0800

    Update WolfKnight.h

--- WolfKnight.h
+++ WolfKnight.h
@@ -10,8 +10,6 @@ public:
     {
         direction_count[LEFT] = 4;
         direction_count[RIGHT] = 4;
-        direction_count[UP] = 4;
-        direction_count[DOWN] = 4;
 
         HealthPoint = 40;
         speed = 2;
