775656ab858f561f679ddecdc8a667a1ede95104
freezexpert <55404744+freezexpert@users.noreply.github.com>
2022-01-17 21:58:26 +0800

    Update GameWindow.h

--- GameWindow.h
+++ GameWindow.h
@@ -1,8 +1,12 @@
 #ifndef MAINWINDOW_H_INCLUDED
 #define MAINWINDOW_H_INCLUDED
-
+#include <stdlib.h>
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <stdio.h>
+#include <string.h>
 #include <vector>
 #include <list>
 #include <time.h>
@@ -11,89 +15,162 @@
 #include "WolfKnight.h"
 #include "CaveMan.h"
 #include "Wolf.h"
-#include "Emerald.h"
 #include "DemonNijia.h"
-#include "Arcane.h"
-#include "Archer.h"
-#include "Canon.h"
-#include "Poison.h"
-#include "Storm.h"
+#include "Tower.h"
 #include "Attack.h"
 #include "Slider.h"
-
+#include "Emerald.h"
+//#include "player.h"
 #define GAME_INIT -1
 #define GAME_SETTING 0
-#define GAME_SELECT 1
-#define GAME_BEGIN 2
-#define GAME_CONTINUE 3
-#define GAME_FAIL 4
-#define GAME_TERMINATE 5
-#define GAME_NEXT_LEVEL 6
-#define GAME_EXIT 7
+#define GAME_MENU 1
+#define GAME_CONTINUE 2
+#define GAME_TURNBACK 3
+#define GAME_TERMINATE 4
+#define GAME_EXIT 5
+#define MAX_BULLET 4
+#define MAX_ENEMY1 10
+#define MAX_ENEMY2 8
+#define MAX_ENEMY3 5
+
+// The active scene id.
 
 // clock rate
-const float FPS = 60;
+const float FPS = 100000;
 
 // total number of level
 const int LevelNum = 4;
+const float MAX_COOLDOWN = 0.2f;
 
 // 1 coin every 2 seconds
 const int CoinSpeed = FPS * 2;
 const int Coin_Time_Gain = 1;
-
+typedef struct {
+    // The center coordinate of the image.
+    float x, y;
+    // The width and height of the object.
+    float w, h;
+    // The velocity in x, y axes.
+    float vx;
+    // The pointer to the object’s image.
+    int hp;
+    int mp;
+    float attack;
+    float speed;
+    float defence;
+    int full_hp;
+    int full_mp;
+    bool hidden;
+    ALLEGRO_BITMAP* img;
+    ALLEGRO_BITMAP* img_tool;
+} MovableObject;
 class GameWindow
 {
 public:
     // constructor
     GameWindow();
     // each process of scene
-    void game_init();
-    void game_reset();
-    void game_play();
-    void game_begin();
 
-    int game_run();
-    int game_update();
-
-    void show_err_msg(int msg);
-    void game_destroy();
-
-    // each drawing scene function
-    void draw_running_map();
-
-    // process of updated event
-    int process_event();
-    // detect if mouse hovers over a rectangle
-    bool mouse_hover(int, int, int, int);
-    // detect if a tower will be constructed on road
-    bool isOnRoad();
-
-    Tower* create_tower(int);
+// Initialize variables and resources.
+// Allows the game to perform any initialization it needs before
+// starting to run.
+    void game_init(void);
+// Process events inside the event queue using an infinity loop.
+    void game_start_event_loop(void);
+// Run game logic such as updating the world, checking for collision,
+// switching scenes and so on.
+// This is called when the game should update its logic.
+    void game_update(void);
+// Draw to display.
+// This is called when the game should draw itself.
+    void game_draw(void);
+// Release resources.
+// Free the pointers we allocated.
+    void game_destroy(void);
+// Function to change from one scene to another.
+    void game_change_scene(int next_scene);
     Monster* create_monster();
-
+    //void draw_movable_object(MovableObject* obj);
+// Load resized bitmap and check if failed.
+    ALLEGRO_BITMAP *load_bitmap_resized(const char *filename, int w, int h);
+// [HACKATHON 3-2]
+// TODO: Declare a function.
+// Determines whether the point (px, py) is in rect (x, y, w, h).
+// Uncomment the code below.
+bool pnt_in_rect(int px, int py, int x, int y, int w, int h);
+void draw_movable_object(MovableObject obj);
+/* Event callbacks. */
+void on_key_down(int keycode);
+void on_mouse_down(int btn, int x, int y);
+void show_err_msg(int msg);
+void game_reset();
+void game_begin();
+void draw_running_map();
 public:
     bool initial = true;
-
+    int active_scene;
+    int role = 1;
+    int score;
+    double last_shoot_timestamp_player = 0;
+    //double last_shoot_timestamp_enemy;
+    //double last_shoot_timestamp_flighter;
+    //double last_shoot_timestamp_monster1;
+    //double last_shoot_timestamp_monster2;
+    // Keyboard state, whether the key is down or not.
+    bool key_state[ALLEGRO_KEY_MAX];
+    // Mouse state, whether the key is down or not.
+    // 1 is for left, 2 is for right, 3 is for middle.
+    bool *mouse_state;
+    // Mouse position.
+    int mouse_x, mouse_y;
+    MovableObject player;
+    MovableObject player_attack[MAX_BULLET];
+    MovableObject enemy1[MAX_ENEMY1];
+    MovableObject enemy2[MAX_ENEMY2];
+    MovableObject enemy3[MAX_ENEMY3];
+    //MovableObject* player;
+    enum {
+    SCENE_MENU = 1,
+    SCENE_START = 2
+    // [HACKATHON 3-7]
+    // TODO: Declare a new scene id.
+    , SCENE_SETTINGS = 3,SCENE_INTRO=4, SCENE_HOME = 5
+};
 private:
     ALLEGRO_BITMAP *icon;
     ALLEGRO_BITMAP *tower[Num_TowerType];
     ALLEGRO_BITMAP *background = NULL;
-
+    ALLEGRO_BITMAP *menu_pic = NULL;
+    ALLEGRO_BITMAP *settings_pic;
+    ALLEGRO_BITMAP *start_button1;
+    ALLEGRO_BITMAP *start_button2;
+    ALLEGRO_BITMAP *role1;
+    ALLEGRO_BITMAP *role2;
+    ALLEGRO_BITMAP *role3;
+    ALLEGRO_BITMAP *role4;
+    ALLEGRO_BITMAP *role1_tool;
+    ALLEGRO_BITMAP *role2_tool;
+    ALLEGRO_BITMAP *role3_tool;
+    ALLEGRO_BITMAP *role4_tool;
+    ALLEGRO_BITMAP *home_pic;
+    ALLEGRO_BITMAP *enemy1_pic;
+    ALLEGRO_BITMAP *enemy2_pic;
+    ALLEGRO_BITMAP *enemy3_pic;
     ALLEGRO_DISPLAY* display = NULL;
     ALLEGRO_FONT *font = NULL;
     ALLEGRO_FONT *Medium_font = NULL;
     ALLEGRO_FONT *Large_font = NULL;
-
-    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    ALLEGRO_EVENT event;
-    ALLEGRO_TIMER *timer = NULL;
-    ALLEGRO_TIMER *monster_pro = NULL;
-
+    ALLEGRO_DISPLAY* game_display;
+    ALLEGRO_EVENT_QUEUE* event_queue;
     ALLEGRO_SAMPLE *sample = NULL;
     ALLEGRO_SAMPLE_INSTANCE *startSound = NULL;
     ALLEGRO_SAMPLE_INSTANCE *clearSound = NULL;
     ALLEGRO_SAMPLE_INSTANCE *failSound = NULL;
     ALLEGRO_SAMPLE_INSTANCE *backgroundSound = NULL;
+    ALLEGRO_TIMER *timer = NULL;
+    ALLEGRO_TIMER *monster_pro = NULL;
+    ALLEGRO_TIMER* game_update_timer;
+
 
     LEVEL *level = NULL;
     Menu *menu = NULL;
@@ -103,7 +180,6 @@ private:
 
     int Monster_Pro_Count = 0;
     int Coin_Inc_Count = 0;
-    int mouse_x, mouse_y;
     int selectedTower = -1, lastClicked = -1;
 
     bool redraw = false;
